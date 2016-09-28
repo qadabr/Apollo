2e453e563f2803a3f166e09ec64f83d6aa06501d
Vladislav Belousov <qadabr@gmail.com>
2016-09-28 15:55:14 +0300

    CMakeLists.txt is smarter now

--- include/Logger.h
+++ include/Logger.h
@@ -7,16 +7,16 @@
 
 #define  LOG_D(...)							\
 	do {								\
-		__android_log_print(ANDROID_LOG_DEBUG, "[%s:ul]:\n",	\
-				    __FILE__, __LINE__);		\
+		__android_log_print(ANDROID_LOG_DEBUG, "[%s:ul %s]:\n",	\
+				    __FILE__, __LINE__, __TIME__);	\
 		__android_log_print(ANDROID_LOG_DEBUG,			\
 				    LOG_TAG, __VA_ARGS__);		\
 	} while (0);							
 
 #define  LOG_E(...)							\
 	do {								\
-		__android_log_print(ANDROID_LOG_ERROR, "[%s:ul]:\n",	\
-				    __FILE__, __LINE__);		\
+		__android_log_print(ANDROID_LOG_ERROR, "[%s:ul %s]:\n",	\
+				    __FILE__, __LINE__, __TIME__);	\
 		__android_log_print(ANDROID_LOG_DEBUG,			\
 				    LOG_TAG, __VA_ARGS__);		\
 	} while (0);							
---
+++ modules/HelloWorld.c
@@ -0,0 +1,11 @@
+#include "Logger.h"
+
+int mod_init(void)
+{
+	LOG_D("Привет, Horizon!\n");
+}
+
+void mod_exit(void)
+{
+	LOG_D("Прощай, Horizon :(\n");
+}
