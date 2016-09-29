61ef4901f54fbb7d13d19c67cc0422b6ab611a86
Vladislav Belousov <qadabr@gmail.com>
2016-09-29 18:38:01 +0300

    Added new logger macros

--- include/Logger.h
+++ include/Logger.h
@@ -5,17 +5,36 @@
 
 #include <android/log.h>
 
-#define LOG_D(M, ...)							\
-	__android_log_print(ANDROID_LOG_DEBUG,				\
-			    LOG_TAG, "[%s:%d][%s]: " M "",		\
-			    __FILE__, __LINE__, __func__,		\
-			    ##__VA_ARGS__)
 
-#define LOG_E(M, ...)							\
-	__android_log_print(ANDROID_LOG_ERROR,				\
-			    LOG_TAG, "[%s:%d][%s]: " M "",		\
-			    __FILE__, __LINE__, __func__,		\
-			    ##__VA_ARGS__)
+#define LOG_E(M, ...) __android_log_print(ANDROID_LOG_ERROR,		\
+					  LOG_TAG,			\
+					  "[%s:%d][%s]: " M "",		\
+					  __FILE__, __LINE__,		\
+					  __func__			\
+					  ##__VA_ARGS__)
+
+
+#define LOG_D(M, ...) __android_log_print(ANDROID_LOG_DEBUG,		\
+					  LOG_TAG,			\
+					  "[%s:%d][%s]: " M "",		\
+					  __FILE__, __LINE__,		\
+					  __func__			\
+					  ##__VA_ARGS__)
+
+#define LOG_W(M, ...) __android_log_print(ANDROID_LOG_WARN,		\
+					  LOG_TAG,			\
+					  "[%s:%d][%s]: " M "",		\
+					  __FILE__, __LINE__,		\
+					  __func__			\
+					  ##__VA_ARGS__)
+
+#define LOG_I(M, ...) __android_log_print(ANDROID_LOG_INFO,		\
+					  LOG_TAG,			\
+					  "[%s:%d][%s]: " M "",		\
+					  __FILE__, __LINE__,		\
+					  __func__			\
+					  ##__VA_ARGS__)
+
 
 
 #endif /* LOGGER_H */
