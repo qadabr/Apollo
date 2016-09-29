a802c7bc24f3efce6b7469bacfdfd911374cf7f2
Vladislav Belousov <qadabr@gmail.com>
2016-09-29 18:39:48 +0300

    Fixed logger build error

--- include/Logger.h
+++ include/Logger.h
@@ -10,7 +10,7 @@
 					  LOG_TAG,			\
 					  "[%s:%d][%s]: " M "",		\
 					  __FILE__, __LINE__,		\
-					  __func__			\
+					  __func__,			\
 					  ##__VA_ARGS__)
 
 
@@ -18,23 +18,22 @@
 					  LOG_TAG,			\
 					  "[%s:%d][%s]: " M "",		\
 					  __FILE__, __LINE__,		\
-					  __func__			\
+					  __func__,			\
 					  ##__VA_ARGS__)
 
 #define LOG_W(M, ...) __android_log_print(ANDROID_LOG_WARN,		\
 					  LOG_TAG,			\
 					  "[%s:%d][%s]: " M "",		\
 					  __FILE__, __LINE__,		\
-					  __func__			\
+					  __func__,			\
 					  ##__VA_ARGS__)
 
 #define LOG_I(M, ...) __android_log_print(ANDROID_LOG_INFO,		\
 					  LOG_TAG,			\
 					  "[%s:%d][%s]: " M "",		\
 					  __FILE__, __LINE__,		\
-					  __func__			\
+					  __func__,			\
 					  ##__VA_ARGS__)
-
 
 
 #endif /* LOGGER_H */
