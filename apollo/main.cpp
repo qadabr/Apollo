3d18e575876e01b255402f9ae078148aa7be1ea8
Vladislav Belousov <qadabr@gmail.com>
2016-10-21 14:15:53 +0300

    Sending and receiving example!

--- apollo/main.cpp
+++ apollo/main.cpp
@@ -6,7 +6,9 @@
 int main(int argc, char** argv)
 {
 	SoundEngine* engine = new SoundEngine();
-	SilverPush silver(engine, 44100, 1000, 10000, 200);
+	
+	SilverPush silver(engine, 48000, 17000, 20000, 200);
+	silver.SendMessage("Hi World");
 	silver.ReceiveMessage();	
 	
 	return 0;
