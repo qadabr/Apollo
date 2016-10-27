593cfec7a96acd21ae6f7f8e66ea3105de46205d
Vladislav Belousov <qadabr@gmail.com>
2016-10-27 16:01:21 +0300

    We can transmit on 500Hz in 19kHz - 22kHz

--- source/main.cpp
+++ source/main.cpp
@@ -7,12 +7,8 @@ int main(int argc, char** argv)
 {
 	SoundEngine* engine = new SoundEngine();
 
-	SilverPush silver(engine, 48000, 19500, 20500, 100);
+	SilverPush silver(engine, 48000, 21500, 22000, 100);
 	silver.PushMessage("Hello!");
-	//silver.PushMessage("hello!");
-	//silver.PushMessage("hello!");
-	//silver.PushMessage("hello!");
-	//silver.PushMessage("hello!");
 	silver.Send();
 	silver.ReceiveMessage();
 	
